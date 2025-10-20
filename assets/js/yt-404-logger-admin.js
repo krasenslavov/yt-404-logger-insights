3e25d51122cc79d0b0c1864e0e71ea51b522dba7
krasenslavov <slavovkrasen@gmail.com>
2025-10-20 03:31:18 +0200

    Update

--- assets/js/yt-404-logger-admin.js
+++ assets/js/yt-404-logger-admin.js
@@ -1,18 +1,18 @@
 /**
  * YT 404 Logger & Insights - Admin JavaScript
  *
+ * @format
  * @package YT_404_Logger_Insights
  */
 
-(function($) {
-	'use strict';
+(function ($) {
+	"use strict";
 
-	$(document).ready(function() {
-
+	$(document).ready(function () {
 		/**
 		 * Handle delete single log entry
 		 */
-		$('.yt-404-delete-log').on('click', function(e) {
+		$(".yt-404-delete-log").on("click", function (e) {
 			e.preventDefault();
 
 			if (!confirm(yt404Logger.confirmDelete)) {
@@ -20,36 +20,36 @@
 			}
 
 			var $button = $(this);
-			var $row = $button.closest('tr');
-			var logId = $button.data('log-id');
+			var $row = $button.closest("tr");
+			var logId = $button.data("log-id");
 
-			$row.addClass('deleting');
-			$button.prop('disabled', true);
+			$row.addClass("deleting");
+			$button.prop("disabled", true);
 
 			$.ajax({
 				url: yt404Logger.ajaxUrl,
-				type: 'POST',
+				type: "POST",
 				data: {
-					action: 'yt_404_logger_delete_log',
+					action: "yt_404_logger_delete_log",
 					nonce: yt404Logger.nonce,
 					log_id: logId
 				},
-				success: function(response) {
+				success: function (response) {
 					if (response.success) {
-						$row.addClass('deleted').fadeOut(400, function() {
+						$row.addClass("deleted").fadeOut(400, function () {
 							$(this).remove();
 							checkEmptyTable();
 						});
 					} else {
 						alert(yt404Logger.errorOccurred);
-						$row.removeClass('deleting');
-						$button.prop('disabled', false);
+						$row.removeClass("deleting");
+						$button.prop("disabled", false);
 					}
 				},
-				error: function() {
+				error: function () {
 					alert(yt404Logger.errorOccurred);
-					$row.removeClass('deleting');
-					$button.prop('disabled', false);
+					$row.removeClass("deleting");
+					$button.prop("disabled", false);
 				}
 			});
 		});
@@ -57,7 +57,7 @@
 		/**
 		 * Handle clear all logs
 		 */
-		$('.yt-404-clear-logs').on('click', function(e) {
+		$(".yt-404-clear-logs").on("click", function (e) {
 			e.preventDefault();
 
 			if (!confirm(yt404Logger.confirmClear)) {
@@ -65,26 +65,26 @@
 			}
 
 			var $button = $(this);
-			$button.prop('disabled', true).text('Clearing...');
+			$button.prop("disabled", true).text("Clearing...");
 
 			$.ajax({
 				url: yt404Logger.ajaxUrl,
-				type: 'POST',
+				type: "POST",
 				data: {
-					action: 'yt_404_logger_clear_logs',
+					action: "yt_404_logger_clear_logs",
 					nonce: yt404Logger.nonce
 				},
-				success: function(response) {
+				success: function (response) {
 					if (response.success) {
 						location.reload();
 					} else {
 						alert(yt404Logger.errorOccurred);
-						$button.prop('disabled', false).text('Clear All Logs');
+						$button.prop("disabled", false).text("Clear All Logs");
 					}
 				},
-				error: function() {
+				error: function () {
 					alert(yt404Logger.errorOccurred);
-					$button.prop('disabled', false).text('Clear All Logs');
+					$button.prop("disabled", false).text("Clear All Logs");
 				}
 			});
 		});
@@ -93,11 +93,10 @@
 		 * Check if table is empty and show message
 		 */
 		function checkEmptyTable() {
-			var $tbody = $('.yt-404-logger-wrap table tbody');
-			if ($tbody.find('tr').length === 0) {
+			var $tbody = $(".yt-404-logger-wrap table tbody");
+			if ($tbody.find("tr").length === 0) {
 				$tbody.html('<tr><td colspan="5">No 404 errors logged yet.</td></tr>');
 			}
 		}
 	});
-
 })(jQuery);
